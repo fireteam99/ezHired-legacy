08d88810c6e6c484a8675a06cb79a20afa80b934
fireteam99 <raymancheungsy@gmail.com>
2019-10-19 15:55:28 -0400

    added options for authentication using facebook and google

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hello from './Hello.jsx';
 import Info from './Info.jsx';
+import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 
 const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
+    <AccountsUIWrapper />
     <Hello />
     <Info />
   </div>
