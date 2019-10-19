cf6644ec1e714075d99a0624301b60d8ad9e4b4d
jon-jew <jonathan.jew@gmail.com>
2019-10-19 19:19:35 -0400

    added dashboard

---
+++ imports/ui/Dashboard/Column.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Dashboard.css';
+
+export default class Column extends Component {
+  render() {
+    return(
+      <div className="column">
+        <div className = "columnHeader">
+          {this.props.title}
+        </div>
+      </div>
+    );
+  }
+}
